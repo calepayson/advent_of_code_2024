8179ce1fabde5cb3c4ca16c92eaefa87319324d0
Cale Smith <calepayson@mac.com>
2024-12-01 10:41:18 -0800

    Update solution.py
    - Calculate the total difference between the two lists.

--- day_1/solution.py
+++ day_1/solution.py
@@ -8,10 +8,19 @@ list_2 = []
 with open("location_ids.txt", 'r') as file:
     for line in file:
         ids = line.strip().split()
-        list_1.append(ids[0])
-        list_2.append(ids[1])
+        list_1.append(int(ids[0]))
+        list_2.append(int(ids[1]))
 
 # Sort each list of locations
 list_1.sort()
 list_2.sort()
+
+# Find the total difference
+total_difference = 0
+for i in range(len(list_1)):
+    difference = abs(list_1[i] - list_2[i])
+    total_difference += difference
+
+# Print the total difference of the two lists
+print(f"Total difference: {total_difference}")
 
